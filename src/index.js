9a7ce18aa529392bf69d7df7f145d33f836952a7
riteek <riteek.s@go-jek.com>
2021-08-15 16:16:42 +0530

    use props in the react component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 
 const color = Math.random() > 0.5 ? 'green' : 'blue'
 
-const App = () => {
+const App = (props) => {
     return (
         <h2 className = 'text-center'>
-            This is the first react component
+            {props.headerMessage}
         </h2>
     )
 }
 
+App.propType = {
+    headerMessage: propTypes.string
+}
+
+App.defaultProps = {
+    headerMessage: "Hello!"
+}
+
 ReactDom.render(
-    <App />,
+    <App headerMessage="This is the first react component with props"/>,
     document.getElementById('root')
 )
